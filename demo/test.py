68bb25b8337ad9f658f41ed67af03530bf6c5d3f
Zulqarnain <76658396+kernel-loophole@users.noreply.github.com>
2024-10-07 23:38:34 +0500

    merage json file

--- demo/test.py
+++ demo/test.py
@@ -1,7 +1,18 @@
-model = LanguageModel("openai-community/gpt2", device_map="auto", dispatch=True)
+from functools import partial
+from safetensors import safe_open
+import torch
+from nnsight import LanguageModel
+from sae_auto_interp.autoencoders.wrapper import AutoencoderLatents
+from sae_auto_interp.autoencoders.OpenAI import Autoencoder
 
-submodule_dict = load_oai_autoencoders(
-    model,
-    layer_list,
-    "sae.safetensors",
-)
+tensors = {}
+with safe_open("sae.safetensors", framework="pt", device=0) as f:
+    for k in f.keys():
+        tensors[k] = f.get_tensor(k)
+
+path = "sae.safetensors" # Change this line to your weights location.
+state_dict = tensors
+ae = Autoencoder.from_state_dict(state_dict=state_dict)
+ae.to("cuda:0")
+
+model = LanguageModel("openai-community/gpt2", device_map="auto", dispatch=True)
